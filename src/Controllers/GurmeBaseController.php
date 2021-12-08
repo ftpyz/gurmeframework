efbc84f5a0695c5641de734a93d817f6d19bc551
Fuat POYRAZ <fuat@fuatpoyraz.com>
2021-12-08 16:13:48 +0300

    first commit

---
+++ src/Controllers/GurmeBaseController.php
@@ -0,0 +1,6 @@
+<?php
+namespace Controllers\GurmeBaseController;
+
+class GurmeBaseController{
+
+}
---
+++ src/Controllers/GurmeWordpressController.php
@@ -0,0 +1,11 @@
+<?php
+
+use Controllers\GurmeBaseController;
+
+class GurmeWordpressController extends GurmeBaseController{
+
+    public function __construct(){
+
+        
+    }
+}

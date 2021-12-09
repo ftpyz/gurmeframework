bed246e50ed8167f8bc89ce1ca2288b18715e8fd
Fuat POYRAZ <fuat@fuatpoyraz.com>
2021-12-09 08:47:56 +0300

    x

--- src/Controllers/SettingsController.php
+++ src/Controllers/SettingsController.php
@@ -4,7 +4,7 @@ use GurmeFramework\Base\BaseController;
 
 class SettingsController extends BaseController{
 
-    public function getSMSProvider(){
+    public function getSMSProviders(){
         $data=array(
             array("name"=>"verimor")
         );
